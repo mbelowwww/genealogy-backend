db5c8e30ca3c8328d86182bb9646d7a1357092c6
Mikhail Belov <mbelowwww@yandex.ru>
2021-02-06 01:46:54 +0400

    Fix bcrypt encoder

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -9,24 +9,24 @@ const registration = (req, res, next) => {
                 error: err
             })
         }
-    })
 
-    let user = new User({
-        username: req.body.username,
-        password: hashedPass,
-        firstName: req.body.firstName,
-        middleName: req.body.middleName,
-        lastName: req.body.lastName,
-        birthday: req.body.birthday
-    })
-    user.save()
-        .then(response => {
+        let user = new User({
+            username: req.body.username,
+            password: hashedPass,
+            firstName: req.body.firstName,
+            middleName: req.body.middleName,
+            lastName: req.body.lastName,
+            birthday: req.body.birthday
+        })
+        user.save()
+            .then(response => {
+                res.json({
+                    message: 'User ' + user.username + ' created!'
+                })
+            }).catch(err => {
             res.json({
-                message: 'User ' + user.username + ' created!'
+                message: 'Error creating user'
             })
-        }).catch(err => {
-        res.json({
-            message: 'Error creating user'
         })
     })
 }
