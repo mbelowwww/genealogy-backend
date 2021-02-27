c6adcdd568a607c5998ebf54038a97444a7854a7
Mikhail Belov <m.belov@haulmont.com>
2021-02-28 00:24:14 +0400

    fix cors problems

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const PORT = process.env.PORT || 3000
 const app = express();
+
+app.use(cors());
 
 app.use(morgan('dev'))
 app.use(bodyParser.json());
@@ -24,6 +27,26 @@ app.use('/api/user', userRoute)
 const nodeRoute = require('./routes/Node')
 app.use('/api/node', nodeRoute)
 
+//cors happy
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 async function start(){
     try {
         await mongoose.connect(
@@ -32,7 +55,7 @@ async function start(){
                 useUnifiedTopology: true,
                 useNewUrlParser: true,
                 useFindAndModify: false
-        })
+            });
         app.listen(PORT, () => {
             console.log('Server has been started on port ', PORT)
         })
